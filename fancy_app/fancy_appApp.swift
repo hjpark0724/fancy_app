1f653299edc2cb9c05bb3c027898ad9274254f92
basker <social724@gmail.com>
2023-05-10 07:35:19 +0900

    Initial Commit

---
+++ fancy_app/fancy_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  fancy_appApp.swift
+//  fancy_app
+//
+//  Created by HYEONJUN PARK on 2023/05/10.
+//
+
+import SwiftUI
+
+@main
+struct fancy_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
